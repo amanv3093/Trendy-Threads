38edafeea39fc7e16f44b8c953abd88e818edd68
aman verma <amanv3093@gmai.com>
2024-04-01 19:27:23 +0530

    add data

--- src/Assets/men.js
+++ src/Assets/men.js
@@ -407,5 +407,617 @@ let mens_product = [
     size: null,
     itemAdded: false,
   },
+  {
+    id: 13,
+    name: "Men's Blue Striped Oversized Shirt",
+    category: "men",
+    new_price: 899,
+    old_price: 1649,
+    off: "45% OFF",
+    cloth_type: "FINE TEXTURE FABRIC",
+    color: "blue",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-blue-striped-oversized-shirt-597301-1704957705-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-blue-striped-oversized-shirt-597301-1704957710-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-blue-striped-oversized-shirt-597301-1704957715-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-blue-striped-oversized-shirt-597301-1704957721-4.jpg",
+      },
+    ],
+    rating: 4.5,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 14,
+    name: "Men's White & Black Mickey Graphic Printed Super Loose Fit Shirt",
+    category: "men",
+    new_price: 899,
+    old_price: 1599,
+    off: "43% OFF",
+    cloth_type: "VISCOSE RAYON",
+    color: "white",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-white-black-mickey-graphic-printed-super-loose-fit-shirt-586228-1704958381-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-white-black-mickey-graphic-printed-super-loose-fit-shirt-586228-1704958386-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-white-black-mickey-graphic-printed-super-loose-fit-shirt-586228-1704958397-4.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-white-black-mickey-graphic-printed-super-loose-fit-shirt-586228-1704958403-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 15,
+    name: "Men's Black All Over Printed Oversized Shirt",
+    category: "men",
+    new_price: 999,
+    old_price: 1699,
+    off: "41% OFF",
+    cloth_type: "VISCOSE RAYON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624422-1710152226-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624422-1710152232-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624422-1710152238-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624422-1710152243-4.jpg",
+      },
+    ],
+    rating: 4.4,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 16,
+    name: "Men's Black All Over Printed Oversized Shirt",
+    category: "men",
+    new_price: 999,
+    old_price: 1899,
+    off: "47% OFF",
+    cloth_type: "VISCOSE RAYON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624419-1710152690-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624419-1710152696-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624419-1710152702-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-all-over-printed-half-sleeve-oversized-shirt-624419-1710152708-4.jpg",
+      },
+    ],
+    rating: 4.5,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 17,
+    name: "Men's Grey All Over Printed Super Loose Fit Shirt",
+    category: "men",
+    new_price: 899,
+    old_price: 1599,
+    off: "43% OFF",
+    cloth_type: "VISCOSE RAYON",
+    color: "gray",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-superloose-aop-shirt-15-586194-1693911006-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-superloose-aop-shirt-15-586194-1693911012-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-superloose-aop-shirt-15-586194-1693911023-4.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-superloose-aop-shirt-15-586194-1693911028-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 18,
+    name: "Men's Black Jap Graphic Printed Oversized Shirt",
+    category: "men",
+    new_price: 1299,
+    old_price: 3399,
+    off: "61% OFF",
+    cloth_type: "100% COTTON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-jap-printed-oversized-shirt-604483-1707200966-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-jap-printed-oversized-shirt-604483-1707200971-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-jap-printed-oversized-shirt-604483-1707200977-4.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-jap-printed-oversized-shirt-604483-1707200982-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 19,
+    name: "Men's Black Checked Oversized Shirt",
+    category: "men",
+    new_price: 999,
+    old_price: 2099,
+    off: "52% OFF",
+    cloth_type: "100% COTTON",
+    color: "gray",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-checked-oversized-shirt-620471-1704957678-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-checked-oversized-shirt-620471-1704957684-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-checked-oversized-shirt-620471-1704957689-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-black-checked-oversized-shirt-620471-1704957694-4.jpg",
+      },
+    ],
+    rating: 4.5,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 20,
+    name: "Men's Multicolor All Over Printed Oversized Shirt",
+    category: "men",
+    new_price: 999,
+    old_price: 1999,
+    off: "50% OFF",
+    cloth_type: "100% COTTON",
+    color: "blue yellow",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-multicolor-all-over-printed-oversized-shirt-620468-1704957822-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-multicolor-all-over-printed-oversized-shirt-620468-1704957827-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-multicolor-all-over-printed-oversized-shirt-620468-1704957833-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/men-s-multicolor-all-over-printed-oversized-shirt-620468-1704957838-4.jpg",
+      },
+    ],
+    rating: 4.5,
+    liked: true,
+    type: "shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 21,
+    name: "Women's Black Boyfriend T-shirt",
+    category: "women",
+    new_price: 399,
+    old_price: 999,
+    off: "60% OFF",
+    cloth_type: "100% COTTON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-boyfriend-t-shirt-103943-1655747735-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-boyfriend-t-shirt-103943-1655747741-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-boyfriend-t-shirt-103943-1655747746-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-boyfriend-t-shirt-103943-1655747757-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 22,
+    name: "Women's Purple Billionaire Girls Club Graphic Printed Oversized T-shirt",
+    category: "women",
+    new_price: 479,
+    old_price: 1299,
+    off: "63% OFF",
+    cloth_type: "100% COTTON",
+    color: "purple",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1708349620-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1694761424-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1694761429-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1694761441-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 23,
+    name: "Women's White Slim Fit T-shirt",
+    category: "women",
+    new_price: 399,
+    old_price: 999,
+    off: "60% OFF",
+    cloth_type: "100% COTTON",
+    color: "white",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1708349620-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1694761424-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1694761429-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-purple-billionaire-girls-club-graphic-printed-oversized-t-shirt-604125-1694761441-5.jpg",
+      },
+    ],
+    rating: 4.4,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 24,
+    name: "Women's Green All Over Printed Oversized T-shirt",
+    category: "women",
+    new_price: 484,
+    old_price: 1149,
+    off: "57% OFF",
+    cloth_type: "100% COTTON",
+    color: "green",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-aop-oversize-t-shirt-7-580365-1688995847-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-aop-oversize-t-shirt-7-580365-1688995852-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-aop-oversize-t-shirt-7-580365-1688995858-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-aop-oversize-t-shirt-7-580365-1688995869-5.jpg",
+      },
+    ],
+    rating: 4.5,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 25,
+    name: "Women's Blue Oversized T-shirt",
+    category: "women",
+    new_price: 489,
+    old_price: 1099,
+    off: "55% OFF",
+    cloth_type: "100% COTTON",
+    color: "blue",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-blue-solid-tshirt-23-582040-1688996238-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-blue-solid-tshirt-23-582040-1688996243-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-blue-solid-tshirt-23-582040-1688996249-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-blue-solid-tshirt-23-582040-1688996259-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 26,
+    name: "Women's Black Believe Cat Graphic Printed Slim Fit T-shirt",
+    category: "women",
+    new_price: 449,
+    old_price: 799,
+    off: "56% OFF",
+    cloth_type: "100% COTTON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/believe-cat-half-sleeve-t-shirt-387069-1656020818-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/believe-cat-half-sleeve-t-shirt-387069-1656020812-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/believe-cat-half-sleeve-t-shirt-387069-1656020828-4.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/believe-cat-half-sleeve-t-shirt-387069-1656020823-3.jpg",
+      },
+    ],
+    rating: 4.4,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 27,
+    name: "Women's Brown Calm Down Graphic Printed Oversized T-shirt",
+    category: "women",
+    new_price: 489,
+    old_price: 1299,
+    off: "62% OFF",
+    cloth_type: "100% COTTON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-brown-calm-down-graphic-printed-oversized-t-shirt-625585-1708436789-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-brown-calm-down-graphic-printed-oversized-t-shirt-625585-1698755496-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-brown-calm-down-graphic-printed-oversized-t-shirt-625585-1698755502-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-brown-calm-down-graphic-printed-oversized-t-shirt-625585-1698755518-6.jpg",
+      },
+    ],
+    rating: 4.5,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 28,
+    name: "Women's Blue Oversized T-shirt",
+    category: "women",
+    new_price: 399,
+    old_price: 999,
+    off: "60% OFF",
+    cloth_type: "100% COTTON",
+    color: "blue",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-blue-oversized-t-shirt-546203-1684862469-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-blue-oversized-t-shirt-546203-1684862474-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-blue-oversized-t-shirt-546203-1684862479-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-blue-oversized-t-shirt-546203-1684862490-5.jpg",
+      },
+    ],
+    rating: 4.3,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 29,
+    name: "Women's Green Boyfriend T-shirt",
+    category: "women",
+    new_price: 399,
+    old_price: 699,
+    off: "42% OFF",
+    cloth_type: "100% COTTON",
+    color: "green",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-boyfriend-t-shirts-3-581053-1684862832-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-boyfriend-t-shirts-3-581053-1684862838-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-boyfriend-t-shirts-3-581053-1684862843-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-boyfriend-t-shirts-3-581053-1684862849-4.jpg",
+      },
+    ],
+    rating: 4.3,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
+  {
+    id: 30,
+    name: "Women's Black Nessy Graphic Printed Oversized T-shirt",
+    category: "women",
+    new_price: 419,
+    old_price: 1299,
+    off: "67% OFF",
+    cloth_type: "100% COTTON",
+    color: "black",
+    image: [
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-nessy-graphic-printed-oversized-t-shirt-584512-1710162642-1.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-nessy-graphic-printed-oversized-t-shirt-13-584512-1695390748-2.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-nessy-graphic-printed-oversized-t-shirt-13-584512-1695390754-3.jpg",
+      },
+      {
+        photo1:
+          "https://images.bewakoof.com/t1080/women-s-black-nessy-graphic-printed-oversized-t-shirt-13-584512-1695390764-5.jpg",
+      },
+    ],
+    rating: 4.6,
+    liked: true,
+    type: "t-shirt",
+    quantity: 1,
+    size: null,
+    itemAdded: false,
+  },
 ];
 export default mens_product;
